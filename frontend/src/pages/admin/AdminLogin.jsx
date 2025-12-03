641aee5a667268793765715e4578698682d26162
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-03 11:49:07 +0000

    added admin loginskelton

---
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -0,0 +1,11 @@
+export default function AdminLogin() {
+  return (
+    <div>
+      <h1>Admin Login</h1>
+
+      <input type="email" placeholder="Admin Email" /><br />
+      <input type="password" placeholder="Password" /><br />
+      <button>Login</button>
+    </div>
+  );
+}
