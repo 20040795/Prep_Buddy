2913ba2b0d1c688b2db51bc849dfd6810cc8ff50
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-03 11:35:32 +0000

    login and register , havent used MUI

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <input type="text" placeholder="Full Name" /><br />
+      <input type="email" placeholder="Email" /><br />
+      <input type="password" placeholder="Password" /><br />
+      <button>Register</button>
+    </div>
+  );
+}
