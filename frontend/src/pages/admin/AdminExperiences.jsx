45542349af5616ece1c1f749edea2b518b3f14a3
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-03 11:46:58 +0000

    added skelton of admin experience

---
+++ frontend/src/pages/admin/AdminExperiences.jsx
@@ -0,0 +1,11 @@
+export default function AdminExperiences() {
+  return (
+    <div>
+      <h1>Admin - Interview Experiences</h1>
+
+      <p></p>
+
+      <button>Delete Experience</button>
+    </div>
+  );
+}
