b80c16b0daa37905c1b240d2a7892204b4cb3105
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-03 11:11:48 +0000

    added the routes in the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Companies from "./pages/Companies";
+import CompanyDetails from "./pages/CompanyDetails";
+import CodingPractice from "./pages/CodingPractice";
+import Forum from "./pages/Forum";
+import AskQuestion from "./pages/AskQuestion";
+import ForumDiscussion from "./pages/ForumDiscussion";
+import AddExperience from "./pages/AddExperience";
+import Profile from "./pages/Profile";
+import GraduatePrograms from "./pages/GraduatePrograms";
+import AdminLogin from "./pages/admin/AdminLogin";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminUsers from "./pages/admin/AdminUsers";
+import AdminQuestions from "./pages/admin/AdminQuestions";
+import AdminExperiences from "./pages/admin/AdminExperiences";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Welcome</div>} />
+      {/* here is the public routes that is where any user can regitser and log in */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+       {/* here is the app routes */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/companies/:companySlug" element={<CompanyDetails />} />
+        <Route path="/coding" element={<CodingPractice />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/forum/ask" element={<AskQuestion />} />
+        <Route path="/forum/:id" element={<ForumDiscussion />} />
+        <Route path="/add-experience" element={<AddExperience />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/graduates" element={<GraduatePrograms />} />
+
+        {/* here is the admin routes */}
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/questions" element={<AdminQuestions />} />
+        <Route path="/admin/experiences" element={<AdminExperiences />} />
       </Routes>
     </BrowserRouter>
   );
