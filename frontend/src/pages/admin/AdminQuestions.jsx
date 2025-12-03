390731b2d76b7595cd01774b5788bfd0f891bd24
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-03 11:50:47 +0000

    skelton:admin questions

---
+++ frontend/src/pages/admin/AdminQuestions.jsx
@@ -0,0 +1,10 @@
+export default function AdminQuestions() {
+  return (
+    <div>
+      <h1>Admin - Forum Questions</h1>
+
+      <p>questions</p>
+      <button>Delete Question</button>
+    </div>
+  );
+}
