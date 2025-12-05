b364ceab46952b7833d8f5012dacea7455bfa25e
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-05 14:49:48 +0000

    navbar was also loading before logging in , removed that by removinf token

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,25 @@
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
 export default function Navbar() {
   const navigate = useNavigate();
+
   const handleLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
     navigate("/");
   };
+
   return (
     <AppBar position="static" sx={{ bgcolor: "#003366" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: "bold" }}>
           DBS Interview Portal
         </Typography>
+
         <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
         <Button color="inherit" component={Link} to="/companies">Companies</Button>
         <Button color="inherit" component={Link} to="/coding">Coding</Button>
         <Button color="inherit" component={Link} to="/forum">Forum</Button>
         <Button color="inherit" component={Link} to="/graduates">Graduates</Button>
+
         <Button 
           variant="outlined" 
           sx={{ ml: 2, borderColor: "white", color: "white" }}
