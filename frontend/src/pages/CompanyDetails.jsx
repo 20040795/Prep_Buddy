ded2c2f70ab067b2eb2e2728f8cd4717de8e1663
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-05 18:58:58 +0000

    sample skelton for company details

--- frontend/src/pages/CompanyDetails.jsx
+++ frontend/src/pages/CompanyDetails.jsx
@@ -1,7 +1,42 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { Box, Typography, Button } from "@mui/material";
+
 export default function CompanyDetails() {
+  const { companySlug } = useParams();
+  const navigate = useNavigate();
+  const experiences = [
+    {
+      id: 1,
+      title: " bla bla",
+      summary: "Good exp",
+    },
+    {
+      id: 2,
+      title: "bla bla 2",
+      summary: "tough",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Company detais</h1>
-    </div>
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        {companySlug.toUpperCase()} - Interview Experiences
+      </Typography>
+
+      <Button 
+        variant="contained" 
+        onClick={() => navigate("/add-experience")}
+        sx={{ mt: 2, mb: 4 }}
+      >
+        Add Your Experience
+      </Button>
+
+      {experiences.map((exp) => (
+        <Box key={exp.id} mt={2}>
+          <Typography variant="h6">{exp.title}</Typography>
+          <Typography>{exp.summary}</Typography>
+        </Box>
+      ))}
+    </Box>
   );
 }
