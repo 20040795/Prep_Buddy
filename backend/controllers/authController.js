8b0deb42f891fee1c0f978ab3b0aadc9e6aad961
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-05 14:18:51 +0000

    user chat gpt https://chatgpt.com/g/g-p-69301ec1ddb881918742fb17c8c492ba-projectreference/c/69301f81-508c-832f-b568-e6de9984198e for the authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,57 @@
+import db from "../config/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+export const register = (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password)
+    return res.status(400).json({ message: "All fields required" });
+  const checkUser = "SELECT * FROM user WHERE email = ?";//here we check wthrn user exists or not
+  db.query(checkUser, [email], async (err, result) => {
+    if (err) return res.status(500).json({ message: "DB error" });
+    if (result.length > 0)
+      return res.status(400).json({ message: "User already exists" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const insertUser =
+      "INSERT INTO user (name, email, password) VALUES (?, ?, ?)";
+    db.query(insertUser, [name, email, hashedPassword], (err) => {
+      if (err) return res.status(500).json({ message: "Insert error" });
+
+      res.json({ message: "User registered successfully" });
+    });
+  });
+};
+
+export const login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).json({ message: "All fields required" });
+
+  const findUser = "SELECT * FROM user WHERE email = ?";
+  db.query(findUser, [email], async (err, result) => {
+    if (err) return res.status(500).json({ message: "DB error" });
+    if (result.length === 0)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const user = result[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+    const token = jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import "./config/db.js";
-import { initializeDatabase } from "./config/initDB.js";
+import { initializeDatabase } from "./config/dbexe.js";
+import authRoutes from "./routes/authRoutes.js";
+
 
 const app = express();
 app.use(cors());
@@ -17,3 +19,4 @@ app.get("/", (req, res) => {
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use("/api/auth", authRoutes);
