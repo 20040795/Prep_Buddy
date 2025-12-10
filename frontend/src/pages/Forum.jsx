5b0273cbec42bc9378190e9dd69c9a1a06acb092
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-10 14:37:39 +0000

    simple react without css

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -1,7 +1,37 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Forum() {
+  const navigate = useNavigate();
+
+  const posts = [
+    { id: 1, title: "How to prepare  for the google interview", tags: "coding" },
+    { id: 2, title: "What questions are asked in Deloitte tech round?", tags: "HR" },
+    { id: 3, title: "Tips for Amazon OA?", tags: "Amazon, OA" },
+  ];
+
   return (
-    <div>
-      <h1>Forum</h1>
+    <div style={{ padding: "20px" }}>
+      <h2>Student Forum</h2>
+
+      <button onClick={() => navigate("/forum/ask")}>
+        Ask a Question
+      </button>
+
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          style={{
+            border: "1px solid #ccc",
+            padding: "10px",
+            marginTop: "10px",
+            cursor: "pointer"
+          }}
+          onClick={() => navigate(`/forum/${post.id}`)}
+        >
+          <h3>{post.title}</h3>
+          <p>Tags: {post.tags}</p>
+        </div>
+      ))}
     </div>
   );
 }
