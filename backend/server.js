63b1cf966acb42193ac4c0f43f073d73eae2281c
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-11 22:14:54 +0000

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,26 +5,39 @@ dotenv.config();
 
 import "./config/db.js";
 import { initializeDatabase } from "./config/dbexe.js";
+
+//routes
 import authRoutes from "./routes/authRoutes.js";
 import companyRoutes from "./routes/companies.routes.js";
 import experienceRoutes from "./routes/experiences.routes.js";
 import codingRoutes from "./routes/coding.routes.js";
+import forumRoutes from "./routes/forum.routes.js";
+import graduateRoutes from "./routes/graduate.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 const app = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
+
+//this creates table
 initializeDatabase();
 
+//index route
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
+
+//api routes
 app.use("/api/auth", authRoutes);
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 app.use("/api/companies", companyRoutes);
 app.use("/api/experiences", experienceRoutes);
 app.use("/api/coding", codingRoutes);
+app.use("/api/forum", forumRoutes);
+app.use("/api/graduates", graduateRoutes);
+app.use("/api/admin", adminRoutes);
 
+//server starts
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
