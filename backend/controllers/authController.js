5d09112b3cd1d3910108c3f6a6012e103c068150
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-11 16:15:40 +0000

    changed bcrypt to bcrptjs, mistake cleared

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 import db from "../config/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 export const register = (req, res) => {
   const { name, email, password } = req.body;
