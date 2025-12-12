ca524d42e88e842be06968249e5d1942d806c889
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-12 12:01:18 +0000

    added the changed in route

--- backend/routes/experiences.routes.js
+++ backend/routes/experiences.routes.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { addExperience, getCompanyExperiences } from "../controllers/experiencesController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { getAllExperiences, deleteExperience } from "../controllers/experiencesController.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
+
+
 const router = express.Router();
 router.post("/", authMiddleware, addExperience);
 router.get("/:companyId", getCompanyExperiences);
+router.get("/all", authMiddleware, adminMiddleware, getAllExperiences);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteExperience);
+
 export default router;
