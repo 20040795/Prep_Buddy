eb1151496309818377a7227bbc9060173a82334f
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-12 15:17:56 +0000

    changed the stats card in component

--- frontend/src/components/StatsCard.jsx
+++ frontend/src/components/StatsCard.jsx
@@ -1,7 +1,8 @@
 import { Card, CardContent, Typography } from "@mui/material";
+
 export default function StatsCard({ title, value }) {
   return (
-    <Card sx={{ width: 220, p: 2 }}>
+    <Card sx={{ p: 2, width: 180 }}>
       <CardContent>
         <Typography variant="h6">{title}</Typography>
         <Typography variant="h4" sx={{ fontWeight: "bold", mt: 1 }}>
