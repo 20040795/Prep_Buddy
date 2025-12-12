6c15a4c0c2950ae7062ce1aa59a174f08fbee518
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-12 12:02:13 +0000

    added the route inside forum routes

--- backend/routes/forum.routes.js
+++ backend/routes/forum.routes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { getPosts, addPost, getPostDetails, addReply } from "../controllers/forumController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
+import { deletePost } from "../controllers/forumController.js";
+
 
 const router = express.Router();
 
@@ -8,5 +11,6 @@ router.get("/", getPosts);
 router.post("/", authMiddleware, addPost);
 router.get("/:id", getPostDetails);
 router.post("/:id/reply", authMiddleware, addReply);
+router.delete("/:id", authMiddleware, adminMiddleware, deletePost);
 
 export default router;
