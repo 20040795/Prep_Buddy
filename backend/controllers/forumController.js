9eb91110cb335dcb46cabc8df769765d54fffcf4
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-13 13:29:03 +0000

    added logic , when forum is added leadership score is updated

--- backend/controllers/forumController.js
+++ backend/controllers/forumController.js
@@ -1,7 +1,8 @@
 import db from "../config/db.js";
+import { updateLeaderboard } from "./leaderboardController.js";
 
 export const getPosts = (req, res) => {
-  db.query("select * from forum_posts", (err, result) => {
+  db.query("SELECT * FROM forum_posts", (err, result) => {
     if (err) return res.status(500).json({ message: "DB error" });
     res.json(result);
   });
@@ -12,11 +13,13 @@ export const addPost = (req, res) => {
   const user_id = req.user.id;
 
   const query = `
-    insert into forum_posts (user_id, title, description, tags)
+    INSERT INTO forum_posts (user_id, title, description, tags)
     VALUES (?, ?, ?, ?)
   `;
+
   db.query(query, [user_id, title, description, tags], (err) => {
     if (err) return res.status(500).json({ message: "Insert error" });
+
     res.json({ message: "Post added successfully" });
   });
 };
@@ -44,19 +47,21 @@ export const getPostDetails = (req, res) => {
   });
 };
 
-
 export const addReply = (req, res) => {
   const { id } = req.params;
-  const { reply } = req.body;   
+  const { reply } = req.body;
   const user_id = req.user.id;
 
   const query = `
-    insert into forum_replies (post_id, user_id, reply)
+    INSERT INTO forum_replies (post_id, user_id, reply)
     VALUES (?, ?, ?)
   `;
 
   db.query(query, [id, user_id, reply], (err) => {
     if (err) return res.status(500).json({ message: "Insert error" });
+
+    updateLeaderboard(user_id, "forum_answers");
+
     res.json({ message: "Reply added" });
   });
 };
@@ -64,12 +69,29 @@ export const addReply = (req, res) => {
 export const deletePost = (req, res) => {
   const { id } = req.params;
 
-  const query = "delete from forum_posts WHERE id = ?";
+  const query = "DELETE FROM forum_posts WHERE id = ?";
 
   db.query(query, [id], (err) => {
     if (err) return res.status(500).json({ message: "Error deleting post" });
-    db.query("delete from forum_replies WHERE post_id = ?", [id]);
+
+    db.query("DELETE FROM forum_replies WHERE post_id = ?", [id]);
 
     res.json({ message: "Post deleted successfully" });
+  });
+};
+
+export const getUserForumPosts = (req, res) => {
+  const { userId } = req.params;
+
+  const query = `
+    SELECT * FROM forum_posts
+    WHERE user_id = ?
+    ORDER BY created_at DESC
+  `;
+
+  db.query(query, [userId], (err, result) => {
+    if (err) return res.status(500).json({ message: "DB error" });
+
+    res.json(result);
   });
 };
