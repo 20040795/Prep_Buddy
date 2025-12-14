5f6e4ea396bfc6b45927bbdaca05ac673697daf6
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-14 00:19:14 +0000

    updated authmiddleware to check for an error

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ export const authMiddleware = (req, res, next) => {
   if (!token) return res.status(401).json({ message: "Not authorized" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Token invalid" });
+    if (err) return res.status(403).json({ message: "Invalid token" });
 
     req.user = decoded;
     next();
