a501e85ff8ca6ca0efcd7f6f18fe4d784dd7a0f3
Aiswarya KS <aiswaryaks2610@gmail.com>
2025-12-15 00:21:08 +0000

    resolving cors error

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config(); // MUST load first
+dotenv.config();
 
-// DB
 import db from "./config/db.js";
 import { initializeDatabase } from "./config/dbexe.js";
 
-// routes
 import authRoutes from "./routes/authRoutes.js";
 import companyRoutes from "./routes/companies.routes.js";
 import experienceRoutes from "./routes/experiences.routes.js";
@@ -23,31 +21,35 @@ import logoRoutes from "./routes/logo.routes.js";
 import statsRoutes from "./routes/stats.routes.js";
 
 const app = express();
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://interviewprepbuddy.netlify.app",
   "https://prep-buddy-9gc6tfcvt-20040795s-projects.vercel.app",
-  "https://prep-buddy.vercel.app",
+  "https://prep-buddy.vercel.app"
 ];
+
 app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
       if (allowedOrigins.includes(origin)) return callback(null, true);
-      return callback(new Error("CORS blocked for origin: " + origin), false);
+      return callback(new Error("CORS blocked: " + origin), false);
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-app.options("*", cors());
 app.use(express.json());
+
 initializeDatabase();
+
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully ✔️");
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/companies", companyRoutes);
 app.use("/api/experiences", experienceRoutes);
@@ -60,11 +62,10 @@ app.use("/api/leaderboard", leaderboardRoutes);
 app.use("/api/graduateapi", graduateAPIRoutes);
 app.use("/api/stats", statsRoutes);
 app.use("/api/logo", logoRoutes);
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`\nServer running on port ${PORT}`);
-  console.log(" Allowed Origins:", allowedOrigins);
-  console.log("Connected to DB:");
-  console.log("   HOST:", process.env.DB_HOST);
-  console.log("   USER:", process.env.DB_USER);
+  console.log(`Server running on port ${PORT}`);
+  console.log("Allowed origins:", allowedOrigins);
 });
